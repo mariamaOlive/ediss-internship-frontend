045ed38960e95f5e5b9863a9c28070be699ec4c4
Mariama Oliveira <mariama.oliveira@yahoo.com.br>
2024-08-21 11:54:41 -0300

    Set dashboard as the main page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { DefaultLayoutComponent } from './layout';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'configuration',
+    redirectTo: 'dashboard',
     pathMatch: 'full'
   },
   {
